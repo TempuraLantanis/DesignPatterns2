dc657f2f0d7ab192f5175fee6619cb8bc30cbe88
Tempura <33734673+TempuraLantanis@users.noreply.github.com>
2023-07-09 23:31:20 +0200

    V1.0.2.1RC

--- src/EngineCreator.java
+++ src/EngineCreator.java
@@ -1,4 +1,8 @@
 public abstract class EngineCreator {
 
+    public void diagnose(){
+        System.out.println("diagnosing");
+    }
+
     public abstract EngineInterface createEngine(String model, int horsePower);
 }
